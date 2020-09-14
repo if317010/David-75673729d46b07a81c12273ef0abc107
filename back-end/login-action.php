5ec920ee5542e747ae62541f700f770aff51742a
if317010 <david.blogitdel@gmail.com>
2020-09-14 15:46:46 +0700

    home

---
+++ back-end/login-action.php
@@ -0,0 +1,14 @@
+<?php include "config.php";
+
+if(isset($_POST['tombol_login'])){
+
+  $username = mysqli_real_escape_string($conn, trim($_POST['username']));
+  if ($_POST['password']==$_POST['repeatPassword'] ) {
+  $password = mysqli_real_escape_string($conn, trim($_POST['password']));
+  }else
+  {
+      echo "<script>alert('Password yang Anda Masukan Tidak Sama');</script>";
+  }
+}
+
+?>
